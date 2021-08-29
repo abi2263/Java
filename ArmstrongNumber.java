75187348e8e60697e8760bcbe202dd0274dc2998
ABINASH R <89682953+abi-7@users.noreply.github.com>
2021-08-29 13:11:14 +0530

    Add files via upload

---
+++ ArmstrongNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int n, num, rem, res = 0;
+        Scanner a = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        n = a.nextInt();
+        num = n;
+        a.close();
+
+        while (n > 0) {
+            rem = n % 10;
+            res += rem * rem * rem;
+            n /= 10;
+        }
+
+        if (num == res) {
+            System.out.print(num + " is an Armstrong number.");
+        } else {
+            System.out.print(num + " is not an Armstrong number.");
+        }
+    }
+}
