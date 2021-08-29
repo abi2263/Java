f6b13cae9d7d6fa15ca314707445928afe1f45d1
ABINASH R <89682953+abi-7@users.noreply.github.com>
2021-08-29 11:48:26 +0530

    Delete summa.java

--- summa.java
+++
@@ -1,6 +0,0 @@
-public class summa {
-    public static void main(String args[]) {  
-                //the below comment will be executed  
-        // \u000d System.out.println("Java comment is executed!!");   
-    }
-}
